72264229075033695979155fe795061175b39841
Drag0n7 <44711325+Drag0n7@users.noreply.github.com>
2020-06-09 17:34:58 -0300

    Add files via upload

---
+++ Nova pasta/js/main.js
@@ -0,0 +1,14 @@
+$('.sp').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+  });
+  $('.sp2').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+  });
